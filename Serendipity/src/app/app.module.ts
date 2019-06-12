6cc00d9832e66a62fbb8f7b3b79dc61b3a52cc5f
Jack Flukinger <jackflukinger@gmail.com>
2019-06-11 19:34:51 -0500

    Switched to SCSS for styling -- offers support for variables, but still similar syntactically to CSS
    Created base components for 3 main stages of site:
    
    interests - user enters genres typically interested in
    
    random - random RRS serves movies to user
    
    serendipitous - Serendipity serves serendipitous movies to user

--- Serendipity/src/app/app.module.ts
+++ Serendipity/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InterestsComponent } from './interests/interests.component';
+import { RandomComponent } from './random/random.component';
+import { SerendipitousComponent } from './serendipitous/serendipitous.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterestsComponent,
+    RandomComponent,
+    SerendipitousComponent
   ],
   imports: [
     BrowserModule
