89f128b5af34bd1815681cc2031c67e57490bc6f
Jack Flukinger <jackflukinger@gmail.com>
2019-06-14 14:23:03 -0500

    Beginning backend REST API creation.

--- Serendipity/src/backend/server.js
+++ Serendipity/src/backend/server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
 app.listen(8000, () => {
-  console.log('Server started!')
+  console.log('Server started!');
+})
+
+app.route('/api/cats').post((req, res) => {
+  res.send(201, req.body)
 })
